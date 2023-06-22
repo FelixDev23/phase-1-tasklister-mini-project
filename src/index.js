7c714143a83bdf08231ea64d1583959cb7c55327
**** <****>
2023-06-23 00:52:43 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createToDo(e.target.new-task-description.value)
+  })
+})
+
+function createToDo(new-task-description) {
+  let p = document.createElement('p')
+  let buttn = document.createElement('button')
+  buttn.addEventListener('click', doDelete)
+  buttn.textContent = 'x'
+  p.textContent = `${todo}  `
+  p.appendChild(buttn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function doDelete(e){
+  e.target.parentNode.remove()
+}
